5d8cff64108f6d2f8a4aa7401d19415bada00586
devlubos <136510726+devlubos@users.noreply.github.com>
2024-12-01 20:44:10 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# flask-template
+# FLASK Template
+
+This project is an example how Flask application can be 
+
+## Installation
+
+1. Download all required packages
+    By using requirements file
+    or manually (list of cmds)
+2. Run application by using cmd (or launching file)
+
+## DOC
+* Flask documentation https://flask.palletsprojects.com/en/stable/
+
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = '1234'
+
+
+@app.route("/")
+def index():
+    flash("what's your name?")
+    return render_template("index.html")
+
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hello there, " + str(request.form['name_input']))
+    return render_template("index.html")
