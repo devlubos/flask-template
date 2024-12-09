132f6ce58e6d78bbb2ce92ceb3ffab0b41b0b918
devlubos <136510726+devlubos@users.noreply.github.com>
2024-12-09 19:49:31 +0100

    add waitress server
    selectable option to launch production server on waitress to handle more traffic

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+from waitress import serve
 from app import create_app
 
 application = create_app()
 
+mode = 'dev'
+
 if __name__ == '__main__':
-    application.run(port=5000, debug=True)
+    if mode == 'dev':
+        application.run(port=5000, debug=True)
+    else:
+        serve(application, host='0.0.0.0', port=5000, threads=2)
